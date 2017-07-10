b499cd577d2e4e6ecd75dd66ec5a40a5c51dd06b
Ranferi Dominguez <ranferi.dominguez@schibsted.com.mx>
2017-07-10 13:14:16 -0500

    First commit

---
+++ MamaDigital/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MamaDigital
+//
+//  Created by RanfeDom on 7/10/17.
+//  Copyright © 2017 Ranfelabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
