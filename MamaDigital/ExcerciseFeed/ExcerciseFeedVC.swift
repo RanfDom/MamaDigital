9ebbe5fca5f3a8187c17ac57db6e22936969d7d9
Ranferi Dominguez <ranfe@Ranferis-MacBook-Pro.local>
2017-07-22 02:35:41 -0500

    Tab controller with tableVC

---
+++ MamaDigital/ExcerciseFeed/ExcerciseFeedVC.swift
@@ -0,0 +1,14 @@
+//
+//  ExcerciseFeed.swift
+//  MamaDigital
+//
+//  Created by Ranferi Dominguez on 7/22/17.
+//  Copyright © 2017 Ranfelabs. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ExcerciseFeedVC : UITableViewController{
+    
+}
