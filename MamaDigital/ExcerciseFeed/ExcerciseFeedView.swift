a8b6939590c4415edc6877af216b1b91915cc961
Ranferi Dominguez <ranfe@Ranferis-MacBook-Pro.local>
2017-07-22 14:39:25 -0500

    WorkoutFeedViewCreation and WorkoutFeedCell

--- MamaDigital/ExcerciseFeed/ExcerciseFeedView.swift
+++ MamaDigital/ExcerciseFeed/ExcerciseFeedView.swift
@@ -9,6 +9,8 @@
 import Foundation
 import UIKit
 
-class ExcerciseFeedVC : UITableViewController{
+class ExcerciseFeedView : UIView{
+    
+    var dataView = [""]
     
 }
---
+++ MamaDigital/ExcerciseFeed/WorkoutFeedCell.swift
@@ -0,0 +1,18 @@
+//
+//  WorkoutFeedCell.swift
+//  MamaDigital
+//
+//  Created by Ranferi Dominguez on 7/22/17.
+//  Copyright © 2017 Ranfelabs. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class WorkoutFeedCell: UITableViewCell {
+ 
+    @IBOutlet weak var backGroundImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+}
