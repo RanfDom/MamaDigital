ace4007ab0d11a1181871e28f8a5f438bf139a43
Ranferi Dominguez <ranf_dom@outlook.com>
2017-07-22 23:24:04 -0500

    Base services and closures are set

--- MamaDigital/API/API_Service.swift
+++ MamaDigital/API/API_Service.swift
@@ -7,7 +7,39 @@
 //
 
 import Foundation
+import Alamofire
 
 class API_Service {
+    let baseURL = "192.168.15.100"
+    
+//    var workout = {() -> [Workout] in return [Workout]()}
+    
+    class func requestDailyWorkout(onSuccess : @escaping (_ success : [Workout]) -> Void, failure: (_ error: NSError?) -> Void) {
+        Alamofire.request("http://192.168.15.100/get/baby/1/workout").response { response in
+            
+            print("Request: \(String(describing: response.request))")
+            print("Response: \(String(describing: response.response))")
+            print("Error: \(String(describing: response.error))")
+            
+        }
+    }
+    
+    class func requestBabyInfo(onSuccess : (_ success : Baby) -> Void, failure: (_ error: NSError?) -> Void) {
+        Alamofire.request("http://192.168.15.100/api/baby/1").responseJSON { response in
+            
+            print("Request: \(String(describing: response.request))")
+            print("Response: \(String(describing: response.response))")
+            print("Error: \(String(describing: response.error))")
+            
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+            
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)") // original server data as UTF8 string
+            }
+            
+        }
+    }
     
 }
