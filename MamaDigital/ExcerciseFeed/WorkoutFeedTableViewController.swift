7f44d85e1e995d930f05df71fe95fc8a90eb484b
Ranferi Dominguez <ranf_dom@outlook.com>
2017-07-23 02:43:00 -0500

    Mock workout service

--- MamaDigital/ExcerciseFeed/WorkoutFeedTableViewController.swift
+++ MamaDigital/ExcerciseFeed/WorkoutFeedTableViewController.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import SDWebImage
 
 class WorkoutFeedTableViewController : UITableViewController{
     
@@ -21,18 +22,18 @@ class WorkoutFeedTableViewController : UITableViewController{
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        API_Service.requestDailyWorkout(onSuccess: { (dailyWorkout) in
-            self.dataView = dailyWorkout
-            print(self.dataView)
-            self.tableView.reloadData()
-        }) { (error) in
-            print(error ?? 0)
-        }
+//        API_Service.requestDailyWorkout(onSuccess: { (dailyWorkout) in
+//            self.dataView = dailyWorkout
+//            print(self.dataView)
+//            self.tableView.reloadData()
+//        }) { (error) in
+//            print(error ?? 0)
+//        }
     }
 
     
     func testApp() {
-        dataView = [Workout(title: "TestTitle")]
+        dataView = [Workout(title: "First Workout", slug: "Test Slug", video_URL: "someURL", img_URL: "https://www.guiainfantil.com/uploads/bebes/mamahablabebe-p.jpg", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi placerat velit nisl, non viverra sapien aliquam ut. Sed varius nisi quis justo ullamcorper, eget fermentum turpis ornare. Morbi ornare luctus nisi, non consectetur metus lacinia rhoncus. Nulla at consectetur ligula, consequat vulputate sapien. Quisque hendrerit nisi in magna maximus faucibus. Nam aliquam, mi sed gravida dictum, dolor tortor vulputate mauris, vel suscipit massa orci et tortor. Nulla vitae tellus consequat, laoreet ex non, vulputate eros. In pharetra orci quam, aliquam sodales nulla bibendum ac. Pellentesque semper lorem eget cursus condimentum.", duration: "150", type: "Motor")]
     }
 }
 
@@ -42,7 +43,7 @@ extension WorkoutFeedTableViewController {//Data Source
         let cell = tableView.dequeueReusableCell(withIdentifier: "workoutCellIdentifier", for: indexPath) as! WorkoutFeedCell
         
         cell.title.text = dataView[indexPath.row].title
-//        cell.backGroundImage = dataView?[indexPath.row].img_URL
+        cell.backGroundImage.sd_setImage(with: URL(string: dataView[indexPath.row].img_URL))
         
         return cell
     }
@@ -56,12 +57,12 @@ extension WorkoutFeedTableViewController {//Data Source
 extension WorkoutFeedTableViewController {//Table Delegate
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let sb = UIStoryboard(name: "Main", bundle: nil)
-        let workoutDetailVC = sb.instantiateViewController(withIdentifier: "workoutDetailIdentifier") as! WorkoutDetailViewController
-        
-        workoutDetailVC.workout = dataView[indexPath.row]
-        
-        self.present(workoutDetailVC, animated: true, completion: nil)
+//        let sb = UIStoryboard(name: "Main", bundle: nil)
+//        let workoutDetailVC = sb.instantiateViewController(withIdentifier: "workoutDetailIdentifier") as! WorkoutDetailViewController
+//        
+//        workoutDetailVC.workout = dataView[indexPath.row]
+//        
+//        self.present(workoutDetailVC, animated: true, completion: nil)
     }
     
 }
