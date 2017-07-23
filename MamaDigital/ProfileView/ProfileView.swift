3d786fdc7f030303d1b3fdd60f65f077030f5f16
dann-sh <danser.hp512@gmail.com>
2017-07-22 19:23:11 -0500

    Create initial profile view config and user and baby entities

---
+++ MamaDigital/ProfileView/ProfileView.swift
@@ -0,0 +1,37 @@
+//
+//  ProfileView.swift
+//  MamaDigital
+//
+//  Created by Daniel Serrano on 22/07/17.
+//  Copyright © 2017 Ranfelabs. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var babiesContainerView: UIView!
+    @IBOutlet weak var babyButton: UIButton!
+    @IBOutlet weak var babyNameLabel: UILabel!
+    
+    var myBaby:Baby?
+    
+    init(frame:CGRect ,_ user : User?) {
+        super.init(frame: frame)
+        myBaby = user?.baby
+        userNameLabel.text = user?.name
+        babyNameLabel.text = myBaby?.name
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func initialViewConfig() {
+       
+    }
+}
