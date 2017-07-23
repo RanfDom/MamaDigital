ebfc4cb2a72bd3b0ffeab6ad3ee7b6f4fe0f17fd
Ranferi Dominguez <ranf_dom@outlook.com>
2017-07-22 21:45:27 -0500

    Initial commit to API Service

---
+++ MamaDigital/API/API_Service.swift
@@ -0,0 +1,13 @@
+//
+//  Services.swift
+//  MamaDigital
+//
+//  Created by Ranferi Dominguez on 7/22/17.
+//  Copyright © 2017 Ranfelabs. All rights reserved.
+//
+
+import Foundation
+
+class API_Service {
+    
+}
