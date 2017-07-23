cce455d1d4f392e4eb22b9a8892f912415b5ec27
Ranferi Dominguez <ranferi.dr@gmail.com>
2017-07-23 04:59:59 -0500

    Daily workout service and adapter fixed and upgraded (#15)

--- MamaDigital/ExcerciseFeed/WorkoutFeedTableViewController.swift
+++ MamaDigital/ExcerciseFeed/WorkoutFeedTableViewController.swift
@@ -22,13 +22,13 @@ class WorkoutFeedTableViewController : UITableViewController{
     }
     
     override func viewDidAppear(_ animated: Bool) {
-//        API_Service.requestDailyWorkout(onSuccess: { (dailyWorkout) in
-//            self.dataView = dailyWorkout
-//            print(self.dataView)
-//            self.tableView.reloadData()
-//        }) { (error) in
-//            print(error ?? 0)
-//        }
+        API_Service.requestDailyWorkout(onSuccess: { (dailyWorkout) in
+            self.dataView = dailyWorkout
+            print(self.dataView)
+            self.tableView.reloadData()
+        }) { (error) in
+            print(error ?? 0)
+        }
     }
 
     
